741d80e04a557f297214c955b0af718d63e71efd
Ewen Henderson <Ewen.Henderson@bankofengland.co.uk>
2018-09-28 11:33:51 +0100

    add ggplotise func

--- R/utils.R
+++ R/utils.R
@@ -25,3 +25,37 @@ show_palette_cols <- function(x) {
     labels = names(x))
 
 }
+
+
+#' Convert list or named vector of colours for use in ggplot manual scale
+#' 
+#' @description Convert colours from a palette into an unnamed vector,
+#' of a specified length, for use in \code{ggplot} - particularly manual
+#' scales e.g. \code{scale_colour_manual()}.
+#'
+#' @param palette List or vector of colours (e.g. \code{boe_cols})
+#' 
+#' @param n Number of colours to return
+#'
+#' @return unnamed vector of n colours
+#' 
+#' @export
+#'
+#' @examples
+#' 
+#' library(ggplot2)
+#'   
+#' ggplot(mtcars, aes(hp, mpg, colour = as.factor(cyl))) +
+#'   geom_point(shape = 16) +
+#'   scale_colour_manual(values = ggplotise(boe_cols, 3))
+
+ggplotise <- function(palette, n = -1) {
+  
+  palette <- unlist(palette)
+
+  if (n > 0) {
+    n <- min(length(palette), n)
+    palette <- palette[1:n]
+  }
+  unname(palette)
+}
