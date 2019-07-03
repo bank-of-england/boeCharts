c2c5aacd39e1c9ac1945e88787284e8b1496aeed
Ewen Henderson <ewen.henderson@bankofengland.co.uk>
2019-07-03 12:17:56 +0100

    add vdiffr to pkg suggests for theme testing. put show_palette_cols func back in

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,28 @@
+#' @title Show colours in a Bank colour palette
+#'
+#' @description \code{\link{show_palette_cols}} produces a pie chart of colours
+#' that make up a \code{boeCharts} palette.
+#'
+#' @param x a named character vector of colours e.g. \code{\link{boe_cols_rich}}
+#'
+#' @return A pie chart displaying the colour scheme.
+#'
+#' @examples
+#'
+#' library(boeCharts)
+#'
+#' show_palette_cols(
+#' x = boe_cols_rich
+#' )
+#'
+#' @export
+show_palette_cols <- function(x) {
+  graphics::pie(
+    rep(1, length(x)),
+    col = x,
+    labels = names(x))
+}
+
 #' Convert list or named vector of colours for use in ggplot manual scale
 #' 
 #' @description Convert colours from a palette into an unnamed vector,
