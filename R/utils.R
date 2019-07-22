75dea7d8902f4c9fccdd151ccc8cdbfa1292179f
Ewen Henderson <ewen.henderson@bankofengland.co.uk>
2019-07-22 09:46:06 +0100

    add startup message and a geom font defaults func

--- R/utils.R
+++ R/utils.R
@@ -74,32 +74,3 @@ left_align_titles <- function(chart) {
   
   grob
 }
-
-# map font on windows sys
-map_font_win <- function(base_family) {
-  
-  if (.Platform$OS.type == "windows") {
-    
-    # check if font already mapped
-    if (base_family %in% names(windowsFonts())) return(NULL)
-    
-    # map font to system
-    args <- list()
-    args[[base_family]] <- windowsFont(base_family)
-    do.call(windowsFonts, args)
-    
-  }
-}
-
-#' Update matching font defaults for text geoms
-#'
-#' Updates [ggplot2::geom_label] and [ggplot2::geom_text] font defaults
-#'
-#' @param family,face,size,color font family name, face, size and color
-#' @export
-update_geom_font_defaults <- function(family="Calibri", face="plain", size=3.5,
-                                      color = "#1e1e1e") {
-  update_geom_defaults("text", list(family=family, face=face, size=size, color=color))
-  update_geom_defaults("label", list(family=family, face=face, size=size, color=color))
-}
-
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,12 @@
 .onAttach <- function(libname, pkgname) {
   
+  packageStartupMessage(
+    "boeCharts: Bank Chart Themes and Styles for 'ggplot2'. 
+     For more information visit:
+     https://almplatform/tfs/UnmanagedCollection/Shared%20Analytical%20Code/_git/boeCharts?path=%2FREADME.md
+    "
+  )
+  
   # map calibri
   map_font_win("Calibri")
 
