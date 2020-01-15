d1406612087aa9e4c7e5fb477c7cbf7f56efb3ba
ewen <ewen.henderson@bankofengland.co.uk>
2020-01-15 11:36:38 +0000

    add specific calibri import helper and font aliases

---
+++ R/calibri.R
@@ -0,0 +1,19 @@
+# load calibri fonts
+import_calibri <- function() {
+  
+  font_import(pattern = "calibri", prompt = FALSE)
+}
+
+#' @rdname Calibri
+#' @md
+#' @title Calibri font name R variable aliases
+#' @description `font_calibri` == "`Calibri`"
+#' @format length 1 character vector
+#' @export
+font_calibri <- "Calibri"
+
+#' @rdname Calibri
+#' @md
+#' @description `font_calibri_light` == "`Calibri Light`"
+#' @export
+font_calibri_light <- "Calibri Light"
