b67f5928e114131c751c144cf1a3cc473ae79616
ewen <ewen.henderson@bankofengland.co.uk>
2020-02-03 14:53:00 +0000

    draft functions for automatic limits for numeric/date axes

---
+++ R/limits.R
@@ -0,0 +1,30 @@
+#' Automatic limits for numeric axes
+#'
+#' @inheritParams scales::breaks_extended
+#' @return
+limits_extended <- function(n = 5, ...) {
+  
+  function(x) {
+    
+    rng <- range(x)
+    
+    breaks <- labeling::extended(rng[1], rng[2], m = n)
+    
+    c(breaks[1], breaks[length(breaks)])
+  }
+}
+
+#' Pretty limits for date/times
+#'
+#' @inheritParams scales::breaks_pretty
+#'
+#' @export
+limits_pretty <- function(n = 5, ...) {
+  
+  function(x) {
+    
+    breaks <- pretty(x, m = n)
+    
+    c(breaks[1], breaks[length(breaks)])
+  }
+}
