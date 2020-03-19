05ab2132bfff690fa806c418cf8cbf38ba4c589b
ewen <ewen.henderson@bankofengland.co.uk>
2020-03-19 17:14:32 +0000

    update mcg theme and clean up cmd checks

--- R/themes.R
+++ R/themes.R
@@ -3,7 +3,6 @@
 #' @description Provides a theme to produce
 #' \href{https://www.bankofengland.co.uk/bank-overground}{Bank Overground} 
 #' style visualisations in \code{ggplot2}.
-#' See \code{left_align_titles()} for full left-alignment of chart titles.
 #'
 #' @details Builds on the 'grammar of graphics' framework implement in
 #' ggplot2. Applying \code{theme_overground()} will adjust graphical parameters
@@ -70,8 +69,7 @@ theme_overground <- function(
   caption_margin = 15,
   strip_text_face = "plain", strip_text_colour = base_colour, strip_text_size = 22, 
   plot_margin = c(10, 10, 10, 10),
-  grid = "X", grid_col = "#C8CCCF", 
-  axis = "X", axis_col = grid_col, 
+  grid = "X", grid_col = "#C8CCCF", axis = "X", axis_col = grid_col, 
   ticks = axis
   ) {
 
@@ -203,141 +201,13 @@ theme_overground <- function(
   p
 }
 
-
-#' A \code{ggplot2} theme for MCG publication-ready charts
-#'
-#' @description Provides a theme to produce
-#' Money & Credit Group (MCG) publication style visualisations in \code{ggplot2}.
-#' See \code{ggplot2::scale_y_continuous} for positioning y-axis on the right.
-#'
-#' @details Builds on the 'grammar of graphics' framework implement in
-#' ggplot2. Applying \code{theme_mcg_pub()} will adjust graphical parameters
-#' to give a plot a feel more in line with MCG publications.
-#' 
-#' @return Will not return anything of itself, but when used in conjuntion
-#' with \code{\link{ggplot}} and (e.g.) \code{\link{geom_point}} from the
-#' package \code{ggplot2}, will apply styling to a plot.
-#'
-#' @param base_size Sets the base size of text for the plot.
-#' Defaults to \code{12}
-#' @param base_colour Sets the default colour of titles and labels.
-#' Must be a named R colour or hexadecimal colour code
-#' (e.g. "#FF0000"). Defaults to \code{#2b2b2b}
-#' @param plot_title_size,plot_title_face plot title size and face
-#' @param plot_title_position Alignment of the plot title/subtitle and caption.
-#' A value of "plot" (the default) means that titles and/or caption are aligned to 
-#' the entire plot (minus any space for margins and plot tag).
-#' A value of "panel" means that titles and/or caption are aligned to the plot panels 
-#' @param subtitle_size,subtitle_face subtitle size and face
-#' @param caption_size,caption_margin,caption_face caption size, margin (top) and face
-#' @param axis_title_size,axis_title_face axis title size and face
-#' @param axis_text_size axis text size
-#' @param strip_text_size,strip_text_face facet label size and face
-#' @param plot_margin plot margin (top, right, bottom, left)
-#' @param axis_col,ticks_col axis & ticks colours; both default to `#2b2b2b`
-#'
-#' @examples \dontrun{
-#'
-#' library(ggplot2)
-#'
-#' ggplot(mtcars, aes(x = mpg, y = wt)) +
-#'    geom_point() +
-#'    labs(title = "A Lovely Plot",
-#'         subtitle = "A thought-provoking subtitle.") +
-#'    theme_mcg_pub() +
-#'    scale_y_continuous(position = "right")
-#' }
-#'
-#' @export
-
-theme_mcg_pub <- function(
-  base_size = 12, base_colour = "#2b2b2b",
-  plot_title_size = 18, plot_title_face = "bold",
-  plot_title_position = c("plot", "panel"),
-  subtitle_size = 12, subtitle_face = "plain",
-  axis_title_size = base_size, axis_title_face = "plain",
-  axis_text_size = base_size,
-  caption_size = 9, caption_margin = 10, caption_face = "plain",
-  strip_text_size = 12, strip_text_face = "plain",
-  plot_margin = c(10, 10, 10, 10),
-  axis_col = "#2b2b2b", ticks_col = "#2b2b2b"
-) {
-
-  title_pos <- match.arg(plot_title_position)
-  
-  # create theme, based on classic
-  theme_classic(base_size = base_size) +
-    
-    theme(
-    
-    # grid
-    panel.grid = element_blank(),
-    
-    # background attributes
-    panel.background = element_blank(),
-    plot.margin = ggplot2::margin(plot_margin),
-
-    # tick marks
-    axis.ticks.length = unit(4, "pt"),
-    axis.ticks = element_line(size = 0.5, colour = ticks_col),
-
-    # axis lines
-    axis.line.x = element_line(size = 0.5, colour = axis_col),
-    axis.line.y = element_line(size = 0.5, colour = axis_col),
-
-    # chart text
-    text = element_text(
-      colour = base_colour, size = base_size,
-      hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.8
-      ),
-    axis.text.x = element_text(colour = base_colour, size = axis_text_size, 
-                               margin = ggplot2::margin(t=3)),
-    axis.text.y = element_text(colour = base_colour, size = axis_text_size, 
-                               margin = ggplot2::margin(r=3)),
-    axis.text.y.right = element_text(colour = base_colour, size = axis_text_size, 
-                                     margin = ggplot2::margin(l=3)),
-    plot.title = element_text(size = plot_title_size, face = plot_title_face, 
-                              hjust = 0, colour = base_colour),
-    plot.title.position = title_pos,
-    plot.subtitle = element_text(colour = base_colour, face = subtitle_face, 
-                                      hjust = 0, size = subtitle_size),
-    plot.caption = element_text(colour = base_colour, hjust = 1, face = caption_face, 
-                                size = caption_size, 
-                                margin = ggplot2::margin(t = caption_margin)),
-    axis.title.x = element_text(angle = 0, vjust = 1, face = axis_title_face, 
-                                size = axis_title_size, colour = base_colour),
-    axis.title.y = element_text(angle = 0, vjust = 1, face = axis_title_face, 
-                                size = axis_title_size, colour = base_colour),
-    axis.title.y.right = element_text(angle = 0, vjust = 1, hjust = 1, 
-                                      face = axis_title_face, 
-                                      size = axis_title_size, colour = base_colour),
-    
-    # legend
-    legend.position = "bottom",
-    legend.title = element_blank(),
-    legend.background = element_blank(),
-    legend.key = element_rect(fill = "transparent", colour = "transparent"),
-    legend.spacing.x = ggplot2::unit(5, 'pt'),
-    legend.spacing.y = ggplot2::unit(5, 'pt'),
-    legend.spacing = ggplot2::unit(5, 'pt'),
-
-    # facet attributes
-    strip.background = element_blank(),
-    strip.text = element_text(color = base_colour, size = strip_text_size, 
-                              face = strip_text_face),
-    strip.text.x = element_text(color = base_colour, size = strip_text_size, 
-                                face = strip_text_face),
-    strip.text.y = element_text(color = base_colour, size = strip_text_size, 
-                                face = strip_text_face, angle = -90)
-  )
-}
-
-
 #' A \code{ggplot2} theme for Monetary Policy Report (MPR) chart styling
 #' 
 #' @description Provides a theme to produce
 #' Monetary Policy Report (FKA Inflation Report) style visualisations in \code{ggplot2}.
-#' See \code{ggplot2::scale_y_continuous} for positioning y-axis on the right.
+#' 
+#' @details See \code{ggplot2::scale_y_continuous} for positioning y-axis on the right.
+#' @details See [ylab_mpr()] for help positioning the y-axis title correctly.
 #' 
 #' @inheritParams theme_overground
 #' @param axis_ticks_length length of axis ticks (in centimetres)
@@ -578,4 +448,120 @@ theme_inflation_report <- function(
             strip_text_size = strip_text_size, 
             plot_margin = plot_margin)
 }
+
+
+#' A \code{ggplot2} theme for MCG publication-ready charts
+#'
+#' @description Provides a theme to produce
+#' Money & Credit Group (MCG) publication style visualisations in \code{ggplot2}.
+#' See \code{ggplot2::scale_y_continuous} for positioning y-axis on the right.
+#'
+#' @details Builds on the 'grammar of graphics' framework implement in
+#' ggplot2. Applying \code{theme_mcg_pub()} will adjust graphical parameters
+#' to give a plot a feel more in line with MCG publications.
+#' 
+#' @return Will not return anything of itself, but when used in conjuntion
+#' with \code{\link{ggplot}} and (e.g.) \code{\link{geom_point}} from the
+#' package \code{ggplot2}, will apply styling to a plot.
+#' 
+#' @inheritParams theme_overground
+#' @param axis_col,ticks_col axis & ticks colours
+#'
+#' @examples \dontrun{
+#'
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(x = mpg, y = wt)) +
+#'    geom_point() +
+#'    labs(title = "A Lovely Plot",
+#'         subtitle = "A thought-provoking subtitle.") +
+#'    theme_mcg_pub() +
+#'    scale_y_continuous(position = "right")
+#' }
+#'
+#' @export
+
+theme_mcg_pub <- function(
+  base_family = "Arial", base_size = 16, base_colour = "#2b2b2b",
+  plot_title_face = "bold", plot_title_size = 18, 
+  plot_title_margin = 10, plot_title_position = c("plot", "panel"),
+  subtitle_size = base_size, subtitle_face = "plain", subtitle_margin = 15,
+  axis_title_size = base_size, axis_title_face = "plain",
+  axis_text_size = base_size,
+  caption_size = 9, caption_margin = 10, caption_face = "plain",
+  strip_text_size = 12, strip_text_face = "plain",
+  plot_margin = c(10, 10, 10, 10),
+  axis_col = base_colour, ticks_col = base_colour
+) {
   
+  title_pos <- match.arg(plot_title_position)
+  
+  # create theme, based on classic
+  theme_classic(base_size = base_size) +
+    
+    theme(
+      
+      # grid
+      panel.grid = element_blank(),
+      
+      # background attributes
+      panel.background = element_blank(),
+      plot.margin = ggplot2::margin(plot_margin),
+      
+      # tick marks
+      axis.ticks.length = unit(4, "pt"),
+      axis.ticks = element_line(size = 0.5, colour = ticks_col),
+      
+      # axis lines
+      axis.line.x = element_line(size = 0.5, colour = axis_col),
+      axis.line.y = element_line(size = 0.5, colour = axis_col),
+      
+      # chart text
+      text = element_text(
+        colour = base_colour, size = base_size, family = base_family,
+        hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.8
+      ),
+      axis.text.x = element_text(colour = base_colour, size = axis_text_size, 
+                                 margin = ggplot2::margin(t=3)),
+      axis.text.y = element_text(colour = base_colour, size = axis_text_size, 
+                                 margin = ggplot2::margin(r=3)),
+      axis.text.y.right = element_text(colour = base_colour, size = axis_text_size, 
+                                       margin = ggplot2::margin(l=3)),
+      plot.title = element_text(size = plot_title_size, face = plot_title_face, 
+                                hjust = 0, colour = base_colour,
+                                margin = ggplot2::margin(b = plot_title_margin)),
+      plot.title.position = title_pos,
+      plot.subtitle = element_text(colour = base_colour, face = subtitle_face, 
+                                   hjust = 0, size = subtitle_size,
+                                   margin = ggplot2::margin(b = subtitle_margin)),
+      plot.caption = element_text(colour = base_colour, hjust = 1, face = caption_face, 
+                                  size = caption_size, 
+                                  margin = ggplot2::margin(t = caption_margin)),
+      axis.title.x = element_text(angle = 0, vjust = 1, face = axis_title_face, 
+                                  size = axis_title_size, colour = base_colour),
+      axis.title.y = element_text(angle = 0, vjust = 1, face = axis_title_face, 
+                                  size = axis_title_size, colour = base_colour),
+      axis.title.y.right = element_text(angle = 0, vjust = 1, hjust = 1, 
+                                        face = axis_title_face, 
+                                        size = axis_title_size, colour = base_colour),
+      
+      # legend
+      legend.position = "bottom",
+      legend.title = element_blank(),
+      legend.background = element_blank(),
+      legend.key = element_rect(fill = "transparent", colour = "transparent"),
+      legend.spacing.x = ggplot2::unit(5, 'pt'),
+      legend.spacing.y = ggplot2::unit(5, 'pt'),
+      legend.spacing = ggplot2::unit(5, 'pt'),
+      
+      # facet attributes
+      strip.background = element_blank(),
+      strip.text = element_text(color = base_colour, size = strip_text_size, 
+                                face = strip_text_face),
+      strip.text.x = element_text(color = base_colour, size = strip_text_size, 
+                                  face = strip_text_face),
+      strip.text.y = element_text(color = base_colour, size = strip_text_size, 
+                                  face = strip_text_face, angle = -90)
+    )
+}
+
--- tests/testthat/test-fonts.R
+++ tests/testthat/test-fonts.R
@@ -1,5 +1,4 @@
 test_that("theme font defaults are applied correctly", {
-  skip("not now")
   theme_over <- theme_overground()
   
   expect_that(theme_over$plot.title$family, equals("Calibri"))
@@ -15,17 +14,17 @@ test_that("theme font defaults are applied correctly", {
 })
 
 test_that("theme font choices are applied correctly", {
-  skip("not now")
-  theme_over <- theme_overground(base_family = "Arial Narrow")
+
+  theme_over <- theme_overground(base_family = "Arial")
   
-  expect_that(theme_over$plot.title$family, equals("Arial Narrow"))
-  expect_that(theme_over$plot.subtitle$family, equals("Arial Narrow"))
-  expect_that(theme_over$axis.title$family, equals("Arial Narrow"))
-  expect_that(theme_over$axis.title.x$family, equals("Arial Narrow"))
-  expect_that(theme_over$axis.title.y$family, equals("Arial Narrow"))
-  expect_that(theme_over$axis.text$family, equals("Arial Narrow"))
-  expect_that(theme_over$axis.text.x$family, equals("Arial Narrow"))
-  expect_that(theme_over$axis.text.y$family, equals("Arial Narrow"))
-  expect_that(theme_over$plot.caption$family, equals("Arial Narrow"))
-  expect_that(theme_over$legend.text$family, equals("Arial Narrow"))
+  expect_that(theme_over$plot.title$family, equals("Arial"))
+  expect_that(theme_over$plot.subtitle$family, equals("Arial"))
+  expect_that(theme_over$axis.title$family, equals("Arial"))
+  expect_that(theme_over$axis.title.x$family, equals("Arial"))
+  expect_that(theme_over$axis.title.y$family, equals("Arial"))
+  expect_that(theme_over$axis.text$family, equals("Arial"))
+  expect_that(theme_over$axis.text.x$family, equals("Arial"))
+  expect_that(theme_over$axis.text.y$family, equals("Arial"))
+  expect_that(theme_over$plot.caption$family, equals("Arial"))
+  expect_that(theme_over$legend.text$family, equals("Arial"))
 })
