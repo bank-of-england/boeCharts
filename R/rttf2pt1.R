ec043a52610ee56ace608e8409177d531ee88fda
ewen <ewen.henderson@bankofengland.co.uk>
2021-12-20 11:01:35 +0000

    fix Rttf2pt1 install

--- R/rttf2pt1.R
+++ R/rttf2pt1.R
@@ -5,12 +5,12 @@
 #' @export
 install_rttf2pt1 <- function() {
   
-  if (utils::packageVersion("rttf2pt1") != "1.3.8") {
+  if (utils::packageVersion("Rttf2pt1") != "1.3.8") {
     try_require("remotes", "install_rttf2pt1")
     remotes::install_version("Rttf2pt1", version = "1.3.8")
   }
   
   message(
-    "rttf2pt1 v1.3.8 installed. Run `boeCharts::import_xxx()` to install fonts"
+    "Rttf2pt1 v1.3.8 installed. Run `boeCharts::import_xxx()` to install fonts"
   )
 }
