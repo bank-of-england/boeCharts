8d9b8e5134844a79eb49d9ec65453264db45b239
Ewen Henderson <Ewen.Henderson@bankofengland.co.uk>
2022-08-12 10:01:18 +0100

    rebasing public

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   
-  packageStartupMessage(
-    "boeCharts: Bank Chart Themes and Styles for 'ggplot2'. 
-     For more information visit:
-     https://almplatform/tfs/UnmanagedCollection/Shared%20Analytical%20Code/_git/boeCharts?path=%2FREADME.md
-    "
-  )
-  
   if (.Platform$OS.type == "windows")  { # nocov start
     if (interactive()) packageStartupMessage("Registering Windows fonts with R")
     extrafont::loadfonts("win", quiet = TRUE)
